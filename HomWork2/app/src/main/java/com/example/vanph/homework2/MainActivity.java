e0651ea4a108cd5cdb936ee455b9155b049a6a50
VanPhi <vanphi96@gmail.com>
2017-09-29 18:47:38 +0700

    Sua lai scroll FOCUS.DOW

--- HomWork2/app/src/main/java/com/example/vanph/homework2/MainActivity.java
+++ HomWork2/app/src/main/java/com/example/vanph/homework2/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.vanph.homework2;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.util.Log;
 import android.view.View;
-import android.support.design.widget.FloatingActionButton;
+import android.widget.ScrollView;
 
 import java.util.Random;
 
@@ -17,13 +14,14 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout ln_imageView;
     private int min =1;
     private int max=5;
-    private int weightsum =1;
+    private ScrollView sv_image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         img_add = (ImageView) findViewById(R.id.img_add);
         ln_imageView = (LinearLayout) findViewById(R.id.ln_imageView);
+        sv_image = (ScrollView) findViewById(R.id.sv_image);
         addListtenners();
     }
 
@@ -35,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
                 Random rd = new Random();
                 int range = max - min + 1;
                 int randomNum = min + rd.nextInt(range);
-                ln_imageView.setWeightSum(weightsum);
                 int width = ln_imageView.getWidth();
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, width/2);
                 layoutParams.setMargins(0,10,0,10);
@@ -47,7 +44,11 @@ public class MainActivity extends AppCompatActivity {
                 abc.setScaleType(ImageView.ScaleType.CENTER);
 
                 ln_imageView.addView(abc);
-                weightsum++;
+                sv_image.post(new Runnable() {
+                    public void run() {
+                        sv_image.fullScroll(sv_image.FOCUS_DOWN);
+                    }
+                });
             }
         });
     }
