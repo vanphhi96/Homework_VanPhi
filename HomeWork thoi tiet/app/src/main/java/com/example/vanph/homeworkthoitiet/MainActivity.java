9643c1a1282b24e41580276c603f16c74ef065c9
DESKTOP-UEPNCKS\vanph <vanphi96@gmail.com>
2017-11-17 23:45:46 +0700

    update

--- HomeWork thoi tiet/app/src/main/java/com/example/vanph/homeworkthoitiet/MainActivity.java
+++ HomeWork thoi tiet/app/src/main/java/com/example/vanph/homeworkthoitiet/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vanph.homeworkthoitiet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +14,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     TextView tv_nhietdo;
     TextView tv_doam;
     Button btn_tim;
@@ -30,12 +32,15 @@ public class MainActivity extends AppCompatActivity {
         btn_tim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                tv_doam.setText("Độ ẩm:");
+                tv_nhietdo.setText("Nhiệt độ:");
                 String vitri = ten.getText().toString();
                 vitri = vitri.toLowerCase();
                  WeatherInterface weatherInterface = RetrofitInstance.getInstace().create(WeatherInterface.class);
                 weatherInterface.getWeather(vitri,"c8d06a30627d02e23c0bc7f7f9e18108").enqueue(new Callback<WeatherResponseJSON>() {
                     @Override
                     public void onResponse(Call<WeatherResponseJSON> call, Response<WeatherResponseJSON> response) {
+                        Log.d(TAG, "onResponse: ");
                         if(response.isSuccessful())
                         {
                             WeatherResponseJSON.ObjectJSON obj = response.body().main;
